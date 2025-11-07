4486214008315b1675f9828eb0e00df608ada493
Prof.  Eduardo Ramos <profeduardo.ramos@fiap.com.br>
2025-11-07 14:24:41 +0000

    Entity Classes

--- better-call-saul-api/src/main/java/com/goodman/api/domain/model/Caso.java
+++ better-call-saul-api/src/main/java/com/goodman/api/domain/model/Caso.java
@@ -5,34 +5,60 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "TB_CASOS")
 @Getter
 @Setter
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Caso {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false, unique = true, length = 30)
     private String numeroProcesso;
 
+    @Column(length = 200)
     private String titulo;
 
+    @Lob // Large Object, mapeia para um tipo TEXT no MySQL
+    @Column(columnDefinition = "TEXT")
     private String descricao;
 
+    @Column(nullable = false)
     private LocalDate dataAbertura;
 
+    @Column
     private LocalDate dataFechamento;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
     private StatusCaso status;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliente_id", nullable = false)
     private Cliente cliente;
 
-    private List<Honorario> honorarios = new ArrayList<>();
+    // private List<Honorario> honorarios = new ArrayList<>();
 
     // Construtor principal
     public Caso(String numeroProcesso, String titulo, String descricao, Cliente cliente) {
@@ -45,12 +71,12 @@ public class Caso {
     }
 
     public void adicionarHonorario(Honorario honorario) {
-        getHonorarios().add(honorario);
+        // getHonorarios().add(honorario);
         honorario.setCaso(this);
     }
 
     public void removerHonorario(Honorario honorario) {
-        getHonorarios().remove(honorario);
+        // getHonorarios().remove(honorario);
         honorario.setCaso(null);
     }
 
